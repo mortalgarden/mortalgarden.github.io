74193b78f137a4de3a03d6458a4bc54c24d3ed20
yaguarasu <futch@duck.com>
2024-06-19 23:26:12 -0600

    create modals for characcters in /oiar.html fix up some navigations few other minor fixes

---
+++ js/modalTMP.js
@@ -0,0 +1,198 @@
+// OIAR EMPLOYEES
+                        const samamaModal = document.getElementById("SamamaKhalid");
+function samamaShow() {
+  samamaModal.showModal();
+}
+function samamaClose() {
+
+  samamaModal.classList.add('hide');
+
+  samamaModal.addEventListener('webkitAnimationEnd', function() {
+    samamaModal.classList.remove('hide');
+    samamaModal.close();
+    samamaModal.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+  }, false);
+}
+                        const aliceModal = document.getElementById("AliceDyer");
+function aliceShow() {
+  aliceModal.showModal();
+}
+function aliceClose() {
+
+  aliceModal.classList.add('hide');
+
+  aliceModal.addEventListener('webkitAnimationEnd', function() {
+    aliceModal.classList.remove('hide');
+    aliceModal.close();
+    aliceModal.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+  }, false);
+}
+                        const gwenModal = document.getElementById("GwenBouchard");
+function gwenShow() {
+  gwenModal.showModal();
+}
+function gwenClose() {
+
+  gwenModal.classList.add('hide');
+
+  gwenModal.addEventListener('webkitAnimationEnd', function() {
+    gwenModal.classList.remove('hide');
+    gwenModal.close();
+    gwenModal.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+  }, false);
+}
+                        const celiaModal = document.getElementById("CeliaRipley");
+function celiaShow() {
+  celiaModal.showModal();
+}
+function celiaClose() {
+
+    celiaModal.classList.add('hide');
+
+    celiaModal.addEventListener('webkitAnimationEnd', function() {
+    celiaModal.classList.remove('hide');
+    celiaModal.close();
+    celiaModal.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+  }, false);
+}
+                        const lenaModal = document.getElementById("LenaKelley");
+function lenaShow() {
+  lenaModal.showModal();
+}
+function lenaClose() {
+
+  lenaModal.classList.add('hide');
+
+  lenaModal.addEventListener('webkitAnimationEnd', function() {
+    lenaModal.classList.remove('hide');
+    lenaModal.close();
+    lenaModal.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+  }, false);
+}
+                        const colinModal = document.getElementById("ColinBecher");
+function colinShow() {
+  colinModal.showModal();
+}
+function colinClose() {
+
+  colinModal.classList.add('hide');
+
+  colinModal.addEventListener('webkitAnimationEnd', function() {
+    colinModal.classList.remove('hide');
+    colinModal.close();
+    colinModal.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+  }, false);
+}
+                        const bonzoModal = document.getElementById("MrBonzo");
+function bonzoShow() {
+  bonzoModal.showModal();
+}
+function bonzoClose() {
+
+  bonzoModal.classList.add('hide');
+
+  bonzoModal.addEventListener('webkitAnimationEnd', function() {
+    bonzoModal.classList.remove('hide');
+    bonzoModal.close();
+    bonzoModal.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+  }, false);
+}
+                          const mowbrayModal = document.getElementById("LadyMowbray");
+function mowbrayShow() {
+  mowbrayModal.showModal();
+}
+function mowbrayClose() {
+
+  mowbrayModal.classList.add('hide');
+
+  mowbrayModal.addEventListener('webkitAnimationEnd', function() {
+    mowbrayModal.classList.remove('hide');
+    mowbrayModal.close();
+    mowbrayModal.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+  }, false);
+}
+                        const teddyModal = document.getElementById("TeddyVaughn");
+function teddyShow() {
+  teddyModal.showModal();
+}
+function teddyClose() {
+
+  teddyModal.classList.add('hide');
+
+  teddyModal.addEventListener('webkitAnimationEnd', function() {
+    teddyModal.classList.remove('hide');
+    teddyModal.close();
+    teddyModal.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+  }, false);
+}
+                        const klausModal = document.getElementById("Klaus");
+function klausShow() {
+  klausModal.showModal();
+}
+function klausClose() {
+
+  klausModal.classList.add('hide');
+
+  klausModal.addEventListener('webkitAnimationEnd', function() {
+    klausModal.classList.remove('hide');
+    klausModal.close();
+    klausModal.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+  }, false);
+}
+                        const freddyModal = document.getElementById("FREDDY");
+function freddyShow() {
+  freddyModal.showModal();
+}
+function freddyClose() {
+
+  freddyModal.classList.add('hide');
+
+  freddyModal.addEventListener('webkitAnimationEnd', function() {
+    freddyModal.classList.remove('hide');
+    freddyModal.close();
+    freddyModal.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+  }, false);
+}
+                        const norrisModal = document.getElementById("NORRIS");
+function norrisShow() {
+  norrisModal.showModal();
+}
+function norrisClose() {
+
+  norrisModal.classList.add('hide');
+
+  norrisModal.addEventListener('webkitAnimationEnd', function() {
+    norrisModal.classList.remove('hide');
+    norrisModal.close();
+    norrisModal.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+  }, false);
+}
+                          const chesterModal = document.getElementById("CHESTER");
+function chesterShow() {
+  chesterModal.showModal();
+}
+function chesterClose() {
+
+  chesterModal.classList.add('hide');
+
+  chesterModal.addEventListener('webkitAnimationEnd', function() {
+    chesterModal.classList.remove('hide');
+    chesterModal.close();
+    chesterModal.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+  }, false);
+}
+                          const augustusModal = document.getElementById("AUGUSTUS");
+function augustusShow() {
+  augustusModal.showModal();
+}
+function augustusClose() {
+
+  augustusModal.classList.add('hide');
+
+  augustusModal.addEventListener('webkitAnimationEnd', function() {
+    augustusModal.classList.remove('hide');
+    augustusModal.close();
+    augustusModal.removeEventListener('webkitAnimationEnd',  arguments.callee, false);
+  }, false);
+}
+// --------------
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// navigation buttons
     const smallSiteNav = document.getElementById("nav-small-s");
     const smallSiteButton = document.getElementById("btn-small-s");
     const smallPageNav = document.getElementById("nav-small-p");
@@ -6,56 +7,57 @@
     const iSmallP = document.getElementById("i-small-p");
     var iF;
     
-    function navSmallS() {
-      smallSiteNav.classList.toggle("active");
-      smallSiteButton.classList.toggle("click");
-      smallPageNav.classList.remove("active");
-      smallPageButton.classList.remove("click");
-        iF = iSmallS.classList.contains("ph-mask-sad");
+  function navSmallS() {
+    smallSiteNav.classList.toggle("active");
+    smallSiteButton.classList.toggle("click");
+    smallPageNav.classList.remove("active");
+    smallPageButton.classList.remove("click");
+      iF = iSmallS.classList.contains("ph-mask-sad");
 
-        if (iF) {
-          iSmallS.classList.replace("ph-mask-sad", "ph-mask-happy");
-        } else {
-          iSmallS.classList.replace("ph-mask-happy", "ph-mask-sad");
-        }
-        iF = iSmallP.classList.contains("ph-eye-closed");
-        if (iF) {
-          iSmallP.classList.replace("ph-eye-closed", "ph-eye");
-        }
-    } 
-    function navSmallP() {
-      smallPageNav.classList.toggle("active");
-      smallPageButton.classList.toggle("click");
-      smallSiteNav.classList.remove("active");
-      smallSiteButton.classList.remove("click");
-        iF = iSmallS.classList.contains("ph-mask-happy");
+      if (iF) {
+        iSmallS.classList.replace("ph-mask-sad", "ph-mask-happy");
+      } else {
+        iSmallS.classList.replace("ph-mask-happy", "ph-mask-sad");
+      }
+      iF = iSmallP.classList.contains("ph-eye-closed");
+      if (iF) {
+        iSmallP.classList.replace("ph-eye-closed", "ph-eye");
+      }
+  } 
+  function navSmallP() {
+    smallPageNav.classList.toggle("active");
+    smallPageButton.classList.toggle("click");
+    smallSiteNav.classList.remove("active");
+    smallSiteButton.classList.remove("click");
+      iF = iSmallS.classList.contains("ph-mask-happy");
 
-        if (iF) {
-          iSmallS.classList.replace("ph-mask-happy", "ph-mask-sad");
-        }
+      if (iF) {
+        iSmallS.classList.replace("ph-mask-happy", "ph-mask-sad");
+      }
 
-        iF = iSmallP.classList.contains("ph-eye");
+      iF = iSmallP.classList.contains("ph-eye");
 
-        if (iF) {
-          iSmallP.classList.replace("ph-eye", "ph-eye-closed");
-        } else {
-          iSmallP.classList.replace("ph-eye-closed", "ph-eye");
-        }
-    } 
+      if (iF) {
+        iSmallP.classList.replace("ph-eye", "ph-eye-closed");
+      } else {
+        iSmallP.classList.replace("ph-eye-closed", "ph-eye");
+      }
+  } 
     const bigSiteNav = document.getElementById("nav-big-s");
     const bigSiteButton = document.getElementById("btn-big-s");
     const bigPageNav = document.getElementById("nav-big-p");
     const iBig = document.getElementById("i-big-s");
 
-    function navBigS() {
-      bigSiteNav.classList.toggle("active");
-      bigPageNav.classList.toggle("inactive");
-      bigSiteButton.classList.toggle("click");
-        iF = iBig.classList.contains("ph-mask-sad");
+  function navBigS() {
+    bigSiteNav.classList.toggle("active");
+    bigPageNav.classList.toggle("inactive");
+    bigSiteButton.classList.toggle("click");
+      iF = iBig.classList.contains("ph-mask-sad");
 
-        if (iF) {
-          iBig.classList.replace("ph-mask-sad", "ph-mask-happy");
-        } else {
-          iBig.classList.replace("ph-mask-happy", "ph-mask-sad");
-        }
-    }
+      if (iF) {
+        iBig.classList.replace("ph-mask-sad", "ph-mask-happy");
+      } else {
+        iBig.classList.replace("ph-mask-happy", "ph-mask-sad");
+      }
+  }
+
